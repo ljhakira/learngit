3375b646cafb6ba77f070815d1ad9567e847bac9
LH <ljhakira@foxmail.com>
2018-03-05 11:37:48 +0800

    first commit

---
+++ swfpost.py
@@ -0,0 +1,10 @@
+import urllib2
+cookies = urllib2.HTTPCookieProcessor()
+opener = urllib2.build_opener(cookies)
+f = opener.open('http://www.xxxx.net/?act=login&name=user01')
+data = '<root>Hello</root>'
+request = urllib2.Request(
+        url     = 'http://www.xxxx.net/?act=send',
+        headers = {'Content-Type' : 'text/xml'},
+        data    = data)
+opener.open(request)
